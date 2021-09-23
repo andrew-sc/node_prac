194b743fdcb902abef2406871cda0e510e8ca139
Andrew <seongchan24@gmail.com>
2021-09-23 21:34:15 +0900

    w5-크롤링, 패키지설치, 상품db에 저장 기능 구현

--- routers/goods.js
+++ routers/goods.js
@@ -4,6 +4,54 @@ const Cart = require("../schemas/cart");
 
 const router = express.Router();
 
+const cheerio = require("cheerio");
+const axios = require("axios");
+const iconv = require("iconv-lite");
+const url = "http://www.yes24.com/24/Category/BestSeller";
+
+
+// 아래는 크롤링한 것을 db에 넣는 코드
+router.get("/goods/add/crawling", async (req, res) => {
+  try {
+    //크롤링 대상 웹사이트 HTML 가져오기
+    await axios({
+      url: url,
+      method: "GET",
+      responseType: "arraybuffer",
+    }).then(async (html) => {
+        //크롤링 코드
+      const content = iconv.decode(html.data, "EUC-KR").toString();
+      const $ = cheerio.load(content);
+      const list = $("ol li");
+
+      await list.each( async (i, tag) => {
+        let desc = $(tag).find("p.copy a").text() 
+        let image = $(tag).find("p.image a img").attr("src")
+        let title = $(tag).find("p.image a img").attr("alt")
+        let price = $(tag).find("p.price strong").text()
+      
+        if(desc && image && title && price){
+          price = price.slice(0,-1).replace(/(,)/g, "")
+          let date = new Date()
+          let goodsId = date.getTime()
+          await Goods.create({
+            goodsId:goodsId,
+            name:title,
+            thumbnailUrl:image,
+            category:"도서",
+            price:price
+          })
+        }
+      });
+    })
+    res.send({ result: "success", message: "크롤링이 완료 되었습니다." });
+
+  } catch (error) {
+    //실패 할 경우 코드
+    res.send({ result: "fail", message: "크롤링에 문제가 발생했습니다", error:error });
+  }
+}); 
+
 router.get("/goods", async (req, res, next) => {
   try {
     const { category } = req.query;
