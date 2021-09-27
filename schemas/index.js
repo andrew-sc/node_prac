16737479f401cd91658a073d8955ad72bf029946
Andrew <seongchan24@gmail.com>
2021-09-27 10:51:59 +0900

    mongoose connect 수정

--- schemas/index.js
+++ schemas/index.js
@@ -1,4 +1,6 @@
-const mongoose = require("mongoose");const connect = () => {
+const mongoose = require("mongoose");
+
+const connect = () => {
   mongoose
     .connect("mongodb://localhost:27017/admin", {
       useNewUrlParser: true,
